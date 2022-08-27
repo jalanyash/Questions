d22b36c08e00d23ec2dba5cd7c7849699e2f54da
jalanyash <57186641+jalanyash@users.noreply.github.com>
2022-08-27 16:59:25 +0530

    Time: 6 ms (24.23%), Space: 8.3 MB (70.57%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,52 +1,23 @@
 class Solution {
 public:
-   void merge(vector<int> &nums,int l, int mid, int r){
-            int j=mid+1;
-            int i=l;
-            int f= l;
-            vector<int> temp(1e5);
-            
-            while(i<=mid && j<=r){
-                if(nums[i]<nums[j]){
-                    temp[f]=nums[i];
-                    i++;
-                }
-                else{
-                    temp[f]=nums[j];
-                    j++;
-                }
-                f++;
-            }
-                    
-                if(i>mid){
-                    while(j<=r){
-                        temp[f]=nums[j];
-                        f++;
-                        j++;
-                    }
-                }else {
-                    while(i<=mid){
-                        temp[f]=nums[i];
-                        f++; i++;
-                    }
-                 }
-                
-                for(int f=l;f<=r;f++)
-                    nums[f]=temp[f];
-            }
-    void mergeSort(vector<int> &nums,int l, int r){
-                if(l<r){
-                    int mid=(l+r)/2;
-                    mergeSort(nums,l,mid);
-                    mergeSort(nums,mid+1,r);
-                    
-                    merge(nums,l,mid,r);
-                }
-            }    
-
     void sortColors(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size()-1);
+        int low=0;
+        int mid=0;
+        int high= (nums.size()-1);
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            
+            else {
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
     }
 };
-
-        
