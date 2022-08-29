b46ddfb047b4153a837609e51875c398ba11e247
jalanyash <57186641+jalanyash@users.noreply.github.com>
2022-08-29 22:07:02 +0530

    Time: 63 ms (48.96%), Space: 19.6 MB (37.60%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -4,24 +4,21 @@ public:
         int n=intervals.size();
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
-        int st = intervals[0][0], ed = intervals[0][1];
+        int st=intervals[0][0], ed=intervals[0][1];
         for(int i=1;i<n;i++){
-            if(intervals[i][0]<=ed)//is merging
-            {
+            if(intervals[i][0]<=ed){
                 ed=max(ed,intervals[i][1]);
             }
-            else//not merging
-            {
+            else{
                 vector<int> tmp(2);
-                tmp[0]=st;tmp[1]=ed;
+                tmp[0]=st, tmp[1]=ed;
                 ans.push_back(tmp);
-                // initialise new pair
                 st=intervals[i][0];
                 ed=intervals[i][1];
             }
         }
         vector<int> tmp(2);
-        tmp[0]=st;tmp[1]=ed;
+        tmp[0]=st, tmp[1]= ed;
         ans.push_back(tmp);
         return ans;
     }
