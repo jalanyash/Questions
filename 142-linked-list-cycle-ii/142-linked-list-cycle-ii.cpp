d19d955b59b1649dd784419f7f7672472788f6fc
jalanyash <57186641+jalanyash@users.noreply.github.com>
2022-09-06 00:42:04 +0530

    Time: 38 ms (5.01%), Space: 9.8 MB (12.13%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -8,36 +8,18 @@
  */
 class Solution {
 public:
-    ListNode* floydCycle(ListNode* head){
-        
-        if(head==NULL || head->next==NULL)
-            return NULL;
-        ListNode*fast=head;
-        ListNode*slow=head;
-        while(fast!=NULL && slow!=NULL){
-            fast=fast->next;
-            if(fast!=NULL){
-                fast=fast->next;
-            }
-            slow=slow->next;
-            
-            if(fast==slow){
-                return slow;
-            }
-        }
-        return NULL;
-    }
     ListNode *detectCycle(ListNode *head) {
         if(head==NULL || head->next==NULL)
             return NULL;
-        ListNode* fastPoint=floydCycle(head);
-        if(!fastPoint)
-            return NULL;
-        ListNode* slow=head;
-        while(slow!=fastPoint){
-            slow=slow->next;
-            fastPoint=fastPoint->next;
+        ListNode* temp=head;
+        map<ListNode*,bool>visited;
+        while(temp!=NULL){
+            if(visited[temp]==true){
+                return temp;
+            }
+            visited[temp]=true;
+            temp=temp->next;
         }
-        return fastPoint;
+        return NULL;
     }
 };
